c163a26e9d2c62ff8f07184051d64e10cb7854d1
Ann Rose Cherian <annrose@MacBook-Pro.local>
2021-05-07 03:13:44 +0530

    create first page of pdf

--- code.py
+++ code.py
@@ -3,3 +3,9 @@ import pandas as pd
 from fpdf import FPDF
 import matplotlib as mpl
 from matplotlib.ticker import ScalarFormatter
+
+#first page of document
+pdf = FPDF(orientation='P', unit='mm', format='A$')
+pdf.add_page()
+pdf.set_font('helvetica', 'bold', 10)
+pdf.set_text_color(255,255,255)
