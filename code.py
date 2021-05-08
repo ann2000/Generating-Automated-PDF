0300e07dbe8c775c071d58eddef44737bc4bc67b
ann2000 <44885115+ann2000@users.noreply.github.com>
2021-05-08 23:58:24 +0530

    Update code.py

--- code.py
+++ code.py
@@ -10,3 +10,10 @@ pdf = FPDF(orientation = 'P', unit = 'mm', format = 'A4')
 pdf.add_page()
 pdf.set_font('helvetica', 'bold', 10)
 pdf.set_text_color(255, 255, 255)
+
+def bar_chart(credit, debit, balance):
+
+    x = [x[0:6] for (x,y) in balance]
+    y1 = [y for (x,y) in credit]
+    y2 = [y for (x,y) in debit]
+    y3 = [y for (x,y) in balance]
